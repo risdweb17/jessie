55500137260a2fb579223f93ba0334e24f8ce855
jessie19970409 <yjiang04@risd.edu>
2017-01-25 21:23:07 -0500

    a lot of changes everywhere

---
+++ exercises/exercise14_the_loop/loop.js
@@ -0,0 +1,9 @@
+
+var text=["#","##","###","####","#####","######","#######"];
+
+for (var i =0; i< text.length; i++){
+   console.log(text[i] +"<br>");}
+   
+document.getElementById("insert").innerHTML=text;
+
+
