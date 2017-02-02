1e94470fad6c56442b91ecb56edc6e653687429c
jessie19970409 <yjiang04@risd.edu>
2017-02-02 11:00:02 -0500

    responsive test 9

--- exercises/Project4_porfolio/toggle.js
+++ exercises/Project4_porfolio/toggle.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-   $("button").click(function(){
+   $(".togglebutton").click(function(){
     $(".timeline").slideToggle('slow');
     });
   
 });
 
 $(document).ready(function(){
-   $("button").click(function(){
-    $("p").slideToggle('slow');
+   $(".togglebutton").click(function(){
+    $(".comment").slideToggle('slow');
     });
   
 });
+
